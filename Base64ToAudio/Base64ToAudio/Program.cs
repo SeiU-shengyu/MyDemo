675d5723be7b94020aa3c281daca63ee95c889ab
pan <1475702015@qq.com>
2020-07-01 16:02:40 +0800

    项目添加

---
+++ Base64ToAudio/Base64ToAudio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Base64ToAudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DecoderBase64File("D:/Mine/MyTest/Base64ToAudio/Resourses/main.json","main");
+            DecoderBase64File("D:/Mine/MyTest/Base64ToAudio/Resourses/track.json", "track");
+            Console.ReadLine();
+        }
+
+        private static void DecoderBase64File(string jsonPath,string audioSavePath)
+        {
+            StreamReader sr = new StreamReader(jsonPath);
+            JsonTextReader jsr = new JsonTextReader(sr);
+            JObject jObject = (JObject)JToken.ReadFrom(jsr);
+            foreach (var json in jObject)
+            {
+                Console.WriteLine(json.Key);
+                string name = json.Key;
+                string base64Str = json.Value.ToString().Split(",")[1];
+                byte[] buffer = Convert.FromBase64String(base64Str);
+                FileStream fs = new FileStream("D:/Mine/MyTest/Base64ToAudio/Resourses/" + audioSavePath + "/" + name, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Write(buffer);
+                fs.Close();
+            }
+        }
+    }
+}
