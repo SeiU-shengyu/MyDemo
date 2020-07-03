7d17525def8473c2848bf0bb3216923a86690bd2
pan <1475702015@qq.com>
2020-07-03 20:24:50 +0800

    寻路

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public void ReadWriteTest()
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory()) + "\\test.txt");
+
+            StreamReader sr = new StreamReader(Directory.GetParent(Directory.GetCurrentDirectory()) + "\\test.json");
+            JsonTextReader jtr = new JsonTextReader(sr);
+            JObject jObject = JToken.ReadFrom(jtr) as JObject;
+            Console.WriteLine(jObject["user"]);
+            sr.Close();
+        }
+
+        public int[][] GetMap()
+        {
+            StreamReader sr = new StreamReader(Directory.GetParent(Directory.GetCurrentDirectory()) + "\\map.txt");
+            string[] strs = sr.ReadToEnd().Split("\n");
+            int[][] map = new int[strs.Length][];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                string[] strs1 = strs[i].Split(",");
+                map[i] = new int[strs.Length];
+                for (int j = 0; j < strs1.Length; j++)
+                {
+                    map[i][j] = int.Parse(strs1[j]);
+                }
+            }
+            return map;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(",");
+
+            Class1 c = new Class1();
+            int[][] map = c.GetMap();
+            GetPath1(map, int.Parse(str[0]), int.Parse(str[1]), int.Parse(str[2]), int.Parse(str[3]));
+            map[int.Parse(str[0])][int.Parse(str[1])] = -1;
+            map[int.Parse(str[2])][int.Parse(str[3])] = -2;
+            for (int i = 0; i < map.Length; i++)
+            {
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] == 1)
+                        Console.Write(".");
+                    else if (map[i][j] == 0)
+                        Console.Write("W");
+                    else if (map[i][j] == -1)
+                        Console.Write("S");
+                    else if (map[i][j] == -2)
+                        Console.Write("E");
+                    else
+                        Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void GetPath1(int[][] map,int curX,int curY,int tarX,int tarY)
+        {
+            if (curX >= map.Length || curY >= map[0].Length || curX < 0 || curY < 0)
+            {
+                return;
+            }
+            else
+            {
+                if ((curX == tarX && curY == tarY))
+                {
+                    map[curX][curY] = 2;
+                    return;
+                }
+                else if (map[curX][curY] == 2 || map[curX][curY] == 0 || map[tarX][tarY] == 2)
+                {
+                    return;
+                }
+                else
+                {
+                    map[curX][curY] = 2;
+                    GetPath1(map, curX + 1, curY, tarX, tarY);
+                    GetPath1(map, curX, curY + 1, tarX, tarY);
+                    GetPath1(map, curX - 1, curY, tarX, tarY);
+                    GetPath1(map, curX, curY - 1, tarX, tarY);
+                }
+            }
+        }
+
+        static void GetPath2(int[][] map)
+        {
+        }
+    }
+}
