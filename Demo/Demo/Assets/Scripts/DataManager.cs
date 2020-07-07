367caa829ca6c832e844df73c41599e3ffb88517
SeiU <949704970@qq.com>
2020-07-07 22:03:13 +0800

    Demo

---
+++ Demo/Demo/Assets/Scripts/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType
+{
+}
+
+public struct ItmeMsg
+{
+    public ItemType type;
+    public int counts;
+    public string describe;
+    public Image icon;
+}
---
+++ Demo/Demo/Assets/Scripts/UIActor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIActor : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler,IPointerUpHandler,IPointerClickHandler,IBeginDragHandler,IDragHandler,IEndDragHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Debug.Log(gameObject.name + "->enter");
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->exit");
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->down");
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->up");
+    }
+
+    public virtual void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->click");
+    }
+
+    public virtual void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->beginDrag");
+    }
+
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->drag");
+    }
+    
+    public virtual void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "->endDrag");
+    }
+}
