91321799b3e513adcb0d346843eef91c1bba6654
SeiU <949704970@qq.com>
2020-07-27 23:05:14 +0800

    后知后觉的心动面试题(屏幕和世界坐标相互转换、按钮事件、向量运算)

---
+++ BallReflect/Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float speed = 10;
+    private Camera mainCamera;
+    private Vector3 moveDir;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        float rate = Random.Range(0.0f, 1.0f);
+        moveDir = (mainCamera.transform.right * rate + mainCamera.transform.up * (1 - rate)).normalized;
+        transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += moveDir * Time.deltaTime * speed;
+        Vector3 screenPos = mainCamera.WorldToScreenPoint(transform.position);
+        if (screenPos.x <= 0 && Vector3.Dot(moveDir, mainCamera.transform.right) <= 0)
+            moveDir = Vector3.Reflect(moveDir, mainCamera.transform.right);
+        if (screenPos.x >= Screen.width && Vector3.Dot(moveDir, mainCamera.transform.right) >= 0)
+            moveDir = Vector3.Reflect(moveDir, -mainCamera.transform.right);
+        if (screenPos.y <= 0 && Vector3.Dot(moveDir, mainCamera.transform.up) <= 0)
+            moveDir = Vector3.Reflect(moveDir, mainCamera.transform.up);
+        if (screenPos.y >= Screen.height && Vector3.Dot(moveDir, mainCamera.transform.up) >= 0)
+            moveDir = Vector3.Reflect(moveDir, -mainCamera.transform.up);
+    }
+}
---
+++ BallReflect/Assets/CreateBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBall : MonoBehaviour {
+    public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateBall()
+    {
+        Instantiate(ball);
+    }
+}
