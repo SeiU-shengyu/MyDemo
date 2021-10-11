44725431389f8467fa699ec79b548549c30195c0
SeiU-shengyu <66616392+SeiU-shengyu@users.noreply.github.com>
2021-10-11 17:10:18 +0800

    行为树、状态机

---
+++ Actor/Actor/BehaviorTree.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorTree
+{
+    enum NodeState
+    {
+        FAILED,
+        SUCCESS,
+        RUNNING
+    }
+    abstract class TreeNode
+    {
+        protected TreeNode pNode;
+        public TreeNode PNode { get { return pNode; } set { pNode = value; } }
+        protected NodeState nState;
+        public NodeState NState { get { return nState; } }
+
+        public virtual TreeNode Enter()
+        {
+            nState = NodeState.RUNNING;
+            return this;
+        }
+        public abstract TreeNode Update();
+        public abstract void Exit();
+    }
+    /// <summary>
+    /// 复合节点,拥有多个子节点
+    /// </summary>
+    class CompositeNode : TreeNode
+    {
+        protected List<TreeNode> childNodes;
+
+        public void AddChildNode(TreeNode treeNode)
+        {
+            if (childNodes == null)
+                childNodes = new List<TreeNode>();
+            childNodes.Add(treeNode);
+            treeNode.PNode = this;
+        }
+        public void RemoveChildNode(TreeNode treeNode)
+        {
+            childNodes.Remove(treeNode);
+        }
+
+        public override TreeNode Enter()
+        {
+            return base.Enter();
+        }
+        public override TreeNode Update()
+        {
+            return this;
+        }
+        public override void Exit()
+        {
+        }
+    }
+    /// <summary>
+    /// 修饰节点,只有一个子节点
+    /// </summary>
+    class DecorateNode : TreeNode
+    {
+        protected TreeNode childNode;
+        public void SetChildNode(TreeNode treeNode)
+        {
+            childNode = treeNode;
+            treeNode.PNode = this;
+        }
+        public void RemoveChildNode()
+        {
+            childNode = null;
+        }
+
+        public override TreeNode Enter()
+        {
+            return base.Enter();
+        }
+        public override TreeNode Update()
+        {
+            return this;
+        }
+        public override void Exit()
+        {
+        }
+    }
+    /// <summary>
+    /// 行为节点,没有子节点
+    /// </summary>
+    class ActionNode : TreeNode
+    {
+        public override TreeNode Enter()
+        {
+            return base.Enter();
+        }
+        public override TreeNode Update()
+        {
+            return this;
+        }
+        public override void Exit()
+        {
+        }
+    }
+    /// <summary>
+    /// 顺序节点,依次执行子节点
+    /// </summary>
+    class SequenceNode : CompositeNode
+    {
+        int curRunningNodeIndex = 0;
+
+        public override TreeNode Enter()
+        {
+            TreeNode runningNode = null;
+            runningNode = base.Enter();
+            if (childNodes != null)
+            {
+                curRunningNodeIndex = 0;
+                runningNode = childNodes[0].Enter();
+            }
+            else
+            {
+                nState = NodeState.SUCCESS;
+            }
+            return runningNode;
+        }
+        public override TreeNode Update()
+        {
+            TreeNode runningNode = null;
+            runningNode = base.Update();
+            if (childNodes[curRunningNodeIndex].NState == NodeState.RUNNING)
+            {
+                runningNode = childNodes[curRunningNodeIndex].Update();
+            }
+            else
+            {
+                childNodes[curRunningNodeIndex].Exit();
+                curRunningNodeIndex++;
+                if (curRunningNodeIndex < childNodes.Count)
+                    runningNode = childNodes[curRunningNodeIndex].Enter();
+                else
+                {
+                    nState = NodeState.SUCCESS;
+                }
+            }
+            return runningNode;
+        }
+    }
+
+    class UntilSuccess : DecorateNode
+    {
+        public override TreeNode Enter()
+        {
+            TreeNode runningNode = null;
+            runningNode = base.Enter();
+            if (childNode != null)
+            {
+                runningNode = childNode.Enter();
+            }
+            else
+            {
+                nState = NodeState.SUCCESS;
+            }
+            return runningNode;
+        }
+        public override TreeNode Update()
+        {
+            TreeNode runningNode = null;
+            runningNode = base.Update();
+            switch (childNode.NState)
+            {
+                case NodeState.RUNNING:
+                    runningNode = childNode.Update();
+                    break;
+                case NodeState.SUCCESS:
+                    childNode.Exit();
+                    nState = NodeState.SUCCESS;
+                    break;
+                case NodeState.FAILED:
+                    runningNode = childNode.Enter();
+                    nState = NodeState.RUNNING;
+                    break;
+            }
+            return runningNode;
+        }
+    }
+
+    class TreeManager
+    {
+        protected TreeNode rootNode;
+        protected TreeNode curRunningNode;
+
+        protected List<TreeNode> nodes;
+        public void AddNode(TreeNode treeNode)
+        {
+            if (nodes == null)
+                nodes = new List<TreeNode>();
+            nodes.Add(treeNode);
+        }
+        public void RemoveNode(TreeNode node)
+        {
+            nodes.Remove(node);
+        }
+
+        public TreeManager(TreeNode rootNode)
+        {
+            this.rootNode = rootNode;
+            curRunningNode = this.rootNode.Enter();
+        }
+
+        public void Update()
+        {
+            if (curRunningNode.NState != NodeState.RUNNING)
+            {
+                if (curRunningNode == rootNode)
+                {
+                    curRunningNode = rootNode.Enter();
+                }
+                else
+                {
+                    curRunningNode = curRunningNode.PNode;
+                }
+            }
+            curRunningNode = curRunningNode.Update();
+        }
+    }
+}
---
+++ Actor/Actor/FSM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM
+{
+    abstract class FSM_State<T>
+    {
+        protected List<FSM_Transformation<T>> transformations;
+        public int ToStateCounts { get { return transformations == null ? 0 : transformations.Count; } }
+        public FSM_Transformation<T> this[int index] { get { return transformations[index]; } }
+        
+        public void AddTransformation(FSM_Transformation<T> transformation)
+        {
+            if (transformations == null)
+                transformations = new List<FSM_Transformation<T>>();
+            transformations.Add(transformation);
+        }
+
+        public abstract void Enter(T owner);
+        public abstract void Update(T owner);
+        public abstract void Exit(T owner);
+    }
+
+    abstract class FSM_Transformation<T>
+    {
+        protected FSM_State<T> toState;
+        public FSM_State<T> ToState { get { return toState; } }
+
+        public FSM_Transformation(FSM_State<T> toState)
+        {
+            this.toState = toState;
+        }
+
+        public abstract bool CheckTransform(T owner);
+    }
+
+    class FSM_Manager<T>
+    {
+        protected T owner;
+        protected FSM_State<T> curState;
+        public FSM_Manager(T owner,FSM_State<T> originalState)
+        {
+            this.owner = owner;
+            curState = originalState;
+            curState.Enter(owner);
+        }
+
+        public void Update()
+        {
+            if (curState != null)
+            {
+                curState.Update(owner);
+                for (int i = 0; i < curState.ToStateCounts; i++)
+                {
+                    if (curState[i].CheckTransform(owner))
+                    {
+                        curState.Exit(owner);
+                        curState = curState[i].ToState;
+                        curState.Enter(owner);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
